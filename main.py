11bb623acfafdd3dff8f2c707fede61c4dfc7c0f
peterchaim <112114915+peterchaim72@users.noreply.github.com>
2022-08-25 18:09:02 +0000

    trying to get the debugger to work

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ def factorial(n):
   if n == 0:
     return 1
   else:
+    x = n+1
+    print("debugging am here " + str(x))
     return n * factorial(n - 1)
-print(factorial(6.5))
+print(factorial(6))
+print("can i stop at a breakpoint please?")
+y = 27
+y = y*2
+print(y)
