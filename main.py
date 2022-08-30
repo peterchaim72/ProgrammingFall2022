cd79285a679bdec352406100bfcac4360a8385a0
peterchaim <112114915+peterchaim72@users.noreply.github.com>
2022-08-30 21:34:36 +0000

    added comments to the replit.com example

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# introductory example of a function from replit.com
+# 8/30/2022 Peter Weinstein. added comments.
+
+# return n! ("n factorial")
 def factorial(n):
   if n == 0:
     return 1
   else:
-    x = n+1
-    print("debugging am here " + str(x))
     return n * factorial(n - 1)
+
+# main function    
 print(factorial(6))
-print("can i stop at a breakpoint please?")
-y = 27
-y = y*2
-print(y)
+
